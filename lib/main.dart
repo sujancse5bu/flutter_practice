a1f2c7d534ef974d0d4c5eae3599004bf21f5569
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2021-12-25 19:58:21 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,16 @@ import 'package:flutter_practice/ui/home.dart';
 void main() {
   // runApp(const Home());
   runApp(MaterialApp(
-    home: const ScaffoldExample(),
+    // home: const ScaffoldExample(),
+    home: const BizCard(),
     theme: ThemeData(
-      textTheme: const TextTheme(
-        bodyText1: TextStyle(color: Colors.white),
-        bodyText2: TextStyle(),
-      ).apply(
-        bodyColor: Colors.white,
-        displayColor: Colors.blue,
-      ),
-    ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.white),
+          bodyText2: TextStyle(),
+        ).apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.blue,
+        ),
+        backgroundColor: Colors.black),
   ));
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,64 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+
+class BizCard extends StatelessWidget {
+  const BizCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('BizCard'),
+        ),
+        backgroundColor: Colors.black,
+        body: Container(
+          alignment: Alignment.center,
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: <Widget>[_getCard(), _getAvatar()],
+          ),
+        ));
+  }
+
+  Container _getCard() {
+    return Container(
+        width: 350,
+        height: 200,
+        margin: const EdgeInsets.all(50),
+        decoration: BoxDecoration(
+            color: Colors.pinkAccent, borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Sujan Mridha',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const Text('sujanmridha.com'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Icon(Icons.facebook_outlined, color: Colors.white),
+                Text('/sujanm.cse5.bu')
+              ],
+            )
+          ],
+        ));
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(color: Colors.redAccent, width: 1.2),
+          image: const DecorationImage(
+              image: NetworkImage('https://picsum.photos/300'),
+              fit: BoxFit.cover)),
+    );
+  }
+}
 
 class ScaffoldExample extends StatelessWidget {
   const ScaffoldExample({Key? key}) : super(key: key);
